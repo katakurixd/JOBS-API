7b76d48e6475f18ad76656188161a88af3c2d5ac
katakurixd <lucasgabriel489@gmail.com>
2022-09-23 07:32:08 -0300

    Swagger docs added

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 require('dotenv').config();
 require('express-async-errors');
+
 // extra security packages
 const helmet = require('helmet')
 const cors = require('cors');
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
+
+//Swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load('./swagger.yaml')
 
 const express = require('express');
 const app = express();
@@ -32,10 +38,13 @@ app.use(helmet())
 app.use(cors())
 app.use(xss())
 
-// routes
 app.get('/', (req, res) => {
-  res.send('jobs api');
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+// routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
 
